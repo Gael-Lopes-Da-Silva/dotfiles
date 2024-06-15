8de3f731f9ca1294803211eeea158fe3d329c599
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-06-15 02:20:33 +0200

    update

--- .config/nvim/lua/configs/mini-animate.lua
+++ .config/nvim/lua/configs/mini-animate.lua
@@ -2,7 +2,6 @@ local now, later = MiniDeps.now, MiniDeps.later
 
 later(function()
     require("mini.animate").setup({
-        cursor = { enable = not vim.g.neovide },
         scroll = { enable = false },
     })
 end)
