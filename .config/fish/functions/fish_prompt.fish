5eaa6b5379bd20187c2281874d33bdfb500dc777
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-06-20 21:38:55 +0200

    update

---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,11 @@
+function fish_prompt
+    set_color normal
+    echo -n '{ '
+
+    set_color $fish_color_cwd
+    echo -n (basename $PWD)
+
+    set_color normal
+    echo -n ' }'
+    echo -n ' '
+end
---
+++ .config/fish/functions/fish_right_prompt.fish
@@ -0,0 +1,13 @@
+function fish_right_prompt
+    set -l date (set_color brgrey)(date "+%R")(set_color normal)
+
+    set -l duration "$cmd_duration$CMD_DURATION"
+    if test $duration -gt 100
+        set duration (math $duration / 1000)s
+    else
+        set duration
+    end
+
+    set_color normal
+    string join " " -- $duration $date ""
+end
