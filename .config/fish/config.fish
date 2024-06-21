9c7ab0872cf71920a1e4b044415c36e59cc8fb57
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-06-21 06:14:30 +0200

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+alias n="nvim"
+alias l="eza -al --icons"
+alias lt="eza -al --icons --tree --level=2 --long"
+
 zoxide init fish | source
