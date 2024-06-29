93e469df0b2587530c223bcb622f587298a9fa40
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-06-29 06:43:15 +0200

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,11 +7,7 @@ end
 
 set fish_greeting
 
-fish_vi_key_bindings
-set -g fish_vi_force_cursor 1
-set fish_cursor_default block
-set fish_cursor_insert line
-set fish_cursor_replace_one underscore
+fish_default_key_bindings
 
 alias c="clear"
 alias g="git"
