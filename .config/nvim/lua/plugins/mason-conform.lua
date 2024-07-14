56e4ff497c729ba654d7a017a5659c15c11eb8db
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-07-14 13:41:12 +0200

    update

--- .config/nvim/lua/plugins/mason-conform.lua
+++ .config/nvim/lua/plugins/mason-conform.lua
@@ -1,9 +1,9 @@
 MiniDeps.later(function()
     require("conform").setup({
-        format_on_save = {
-            timeout_ms = 500,
-            lsp_fallback = true,
-        },
+        -- format_on_save = {
+        --     timeout_ms = 500,
+        --     lsp_fallback = true,
+        -- },
     })
     require("mason-conform").setup({})
 end)
