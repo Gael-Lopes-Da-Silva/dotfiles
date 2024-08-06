8c7761ad978444bb7a20ee775a9c5d5f358d5299
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-08-06 16:08:33 +0200

    update

--- .config/i3blocks/battery.sh
+++ .config/i3blocks/battery.sh
@@ -3,28 +3,23 @@ BATTERY=$(acpi -b | grep -E -o '[0-9][0-9]?%')
 BACKGROUND=""
 ICON=""
 
+if [[ $BATTERY -eq "" ]]; then
+    exit 1
+fi
 
-BAT=$(acpi -b | grep -E -o '[0-9][0-9]?%')
+if [[ $STATUS = "Discharging," ]]; then
+    [[ $BATTERY -le 15 ]] && BACKGROUND="#FF0000" || BACKGROUND=""
+fi
 
-echo "Battery: $BAT"
-echo "BAT: $BAT"
+if [[ $STATUS = "Charging," ]]; then
+    [[ $BATTERY -ge 95 ]] && BACKGROUND="#00FF00" || BACKGROUND=""
+fi
 
-[ ${BAT%?} -le 53 ] && exit 33
+[[ $STATUS = "Charging," ]] && ICON="󱐋" || ICON="󰁹"
 
-# if [[ $BATTERY -eq "" ]]; then
-#     exit 1
-# fi
-#
-# if [[ $STATUS = "Discharging," ]]; then
-#     [[ $BATTERY -le 15 ]] && BACKGROUND="#FF0000" || BACKGROUND=""
-# fi
-#
-# if [[ $STATUS = "Charging," ]]; then
-#     [[ $BATTERY -ge 95 ]] && BACKGROUND="#00FF00" || BACKGROUND=""
-# fi
-#
-# [[ $STATUS = "Charging," ]] && ICON="󱐋" || ICON="󰁹"
-#
-# echo " $ICON $BATTERY "
+echo " $ICON $BATTERY "
+
+[ ${BATTERY%?} -le 5 ] && exit 33
+[ ${BATTERY%?} -le 20 ] && echo "#FF8000"
 
 exit 0
