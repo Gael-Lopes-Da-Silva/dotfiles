4127c3452a9774cd80a09f506d9b0f0a3fcf451e
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-08-20 02:12:16 +0200

    update

--- .scripts/install_packages.sh
+++ .scripts/install_packages.sh
@@ -1,34 +1,26 @@
 #!/bin/bash
 
-function install_yay() {
-    local YAY=$(pacman -Qq yay)
-
-    if [[ ! $YAY = "yay" ]]; then
-        sudo pacman -S --needed --noconfirm git base-devel
-        git clone https://aur.archlinux.org/yay.git
-        cd yay
-        makepkg -si --noconfirm
-        cd ~
-        rm -rf yay
-        echo "[+] Yay installed successfully"
-    else
-        echo "[+] Yay already installed"
-    fi
-}
-
-function install_packages() {
-    sudo pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra \
-    kitty neovim zsh ripgrep stow man-db man-pages firefox discord
-    echo "[+] Installed all packages successfully"
-}
-
-function install_i3packages() {
-    sudo pacman -S --noconfirm i3-wm i3blocks i3lock xclip xorg-xsetroot maim xdotool acpi
-    echo "[+] Installed all i3 packages successfully"
-}
-
 cd ~
 
-install_yay
-install_packages
-install_i3packages
+YAY=$(pacman -Qq yay)
+
+if [[ ! $YAY = "yay" ]]; then
+    sudo pacman -S --needed --noconfirm git base-devel
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si --noconfirm
+    cd ~
+    rm -rf yay
+    echo "[+] Yay installed successfully"
+else
+    echo "[+] Yay already installed"
+fi
+
+sudo pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra \
+kitty neovim zsh ripgrep stow man-db man-pages firefox discord
+
+echo "[+] Installed all packages successfully"
+
+sudo pacman -S --noconfirm i3-wm i3blocks i3lock xclip xorg-xsetroot maim xdotool acpi
+
+echo "[+] Installed all i3 packages successfully"
--- .scripts/setup_v4l2loopback.sh
+++ .scripts/setup_v4l2loopback.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-function install_v4l2loopback() {
-    sudo pacman -S --needed --noconfirm base-devel linux-headers
-    sudo pacman -S --noconfirm v4l2loopback-dkms v4l2loopback-utils
-    echo "[+] Installed v4l2loopback successfully"
-    sudo modprobe v4l2loopback
-    echo "[+] Enabled v4l2loopback successfully"
-}
-
 cd ~
 
-install_v4l2loopback
+sudo pacman -S --needed --noconfirm base-devel linux-headers
+sudo pacman -S --noconfirm v4l2loopback-dkms v4l2loopback-utils
+
+echo "[+] Installed v4l2loopback successfully"
+
+sudo modprobe v4l2loopback
+
+echo "[+] Enabled v4l2loopback successfully"
