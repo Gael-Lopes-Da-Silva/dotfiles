057c5273ab5121af1cce0a13043d072e6ef59919
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-08-26 09:03:20 +0200

    update

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,17 +1,22 @@
 MiniDeps.now(function()
+	local group = vim.api.nvim_create_augroup("UserConfig", { clear = true })
+
 	vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
 		desc = "Check if we need to reload the file when it changed",
+		group = group,
 		command = "checktime",
 	})
 
 	vim.api.nvim_create_autocmd("TermOpen", {
 		desc = "Don't show any numbers inside terminals",
 		pattern = { "term://*" },
+		group = group,
 		command = "setlocal signcolumn=no nonumber norelativenumber | setfiletype terminal",
 	})
 
-	vim.api.nvim_create_autocmd({ "VimResized" }, {
+	vim.api.nvim_create_autocmd("VimResized", {
 		desc = "Resize splits if window got resized",
+		group = group,
 		callback = function()
 			local current_tab = vim.fn.tabpagenr()
 			vim.cmd("tabdo wincmd =")
@@ -21,6 +26,7 @@ MiniDeps.now(function()
 
 	-- vim.api.nvim_create_autocmd("BufWritePre", {
 	-- 	desc = "Format the buffer when saved",
+	-- 	group = group,
 	-- 	callback = function()
 	-- 		vim.lsp.buf.format({ timeout_ms = 3000 })
 	-- 	end,
