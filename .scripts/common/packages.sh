e33644a17d7669afec55aa46e2e4e7cf75da2f42
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-08 17:46:50 +0200

    Update

--- .scripts/common/packages.sh
+++ .scripts/common/packages.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-pacman -S --noconfirm stow neovim lazygit zsh feh kitty firfoxe
+pacman -S --noconfirm stow neovim lazygit zsh feh kitty firefox
 pacman -S --noconfirm noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk ttf-cascadia-code
 pacman -S --noconfirm papirus-icon-theme
