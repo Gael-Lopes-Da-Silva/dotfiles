5da36e1c5e8756aee13d9255a4910923cf51ee31
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-09 18:03:44 +0200

    Upadte

--- .scripts/common/packages.sh
+++ .scripts/common/packages.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-pacman -S --noconfirm stow neovim lazygit zsh feh kitty firefox
+pacman -S --noconfirm stow neovim lazygit nushell feh kitty firefox
 pacman -S --noconfirm noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk ttf-cascadia-code
 pacman -S --noconfirm papirus-icon-theme
+
+chsh -s /usr/bin/nu
