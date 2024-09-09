1b3cc9cb16211166f0efc6b1b15fea2f51e8799b
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-09 19:18:44 +0200

    Update

--- .scripts/common/ly.sh
+++ .scripts/common/ly.sh
@@ -6,4 +6,4 @@ sed -i "s|sleep_cmd = null|sleep_cmd = systemctl suspend|" /etc/ly/config.ini
 
 pacman -S --noconfirm terminus-font
 setfont ter-132n
-echo "FONT=ter-132n" >> /etc/vconsole.conf
+printf "FONT=ter-132n" >> /etc/vconsole.conf
--- .scripts/utils/grub.sh
+++ .scripts/utils/grub.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 grub-mkfont -s 32 -o /boot/grub/fonts/CascadiaMonoNF.pf2 /usr/share/fonts/TTF/CascadiaMonoNF.ttf
-echo 'FONT="/boot/grub/fonts/CascadiaMonoNF.pf2"' >> /etc/default/grub
+printf 'FONT="/boot/grub/fonts/CascadiaMonoNF.pf2"' >> /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfn
