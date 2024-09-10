fbc0df7af0d2f04da1195db735b3321e517326af
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-10 23:20:49 +0200

    Update

--- .scripts/driver/nvidia.sh
+++ .scripts/driver/nvidia.sh
@@ -2,12 +2,9 @@
 
 pacman -S --noconfirm nvidia nvidia-settings
 
-sed -i "s|MODULES=(btrfs)|MODULES=(btrfs nvidia nvidia_modeset nvidia_uvm nvidia_drm)|"
+sed -i "s|MODULES=(btrfs)|MODULES=(btrfs nvidia nvidia_modeset nvidia_uvm nvidia_drm asus_wmi)|" /etc/mkinitcpio.conf
 
 mkdir /etc/pacman.d/hooks/
 echo -e "[Trigger]\nOperation=Install\nOperation=Upgrade\nOperation=Remove\nType=Package\nTarget=nvidia\nTarget=linux\n[Action]\nDescription=Updating NVIDIA module in initcpio\nDepends=mkinitcpio\nWhen=PostTransaction\nNeedsTargets\nExec=/bin/sh -c 'while read -r trg; do case \$trg in linux*) exit 0; esac; done; /usr/bin/mkinitcpio -P'\n" >> /etc/pacman.d/hooks/nvidia.hook
 
 nvidia-xconfig
-
-modprobe nvidia_drm modeset=1
-modprobe nvidia_drm fbdev=1
