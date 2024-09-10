6c97c0fd9a24f718fe24627ad87c8ae1af331f4f
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-10 19:53:51 +0200

    Update

--- .config/i3blocks/scripts/date.sh
+++ .config/i3blocks/scripts/date.sh
@@ -6,8 +6,12 @@ I=""
 
 {
     B="19/06"
+    T="11/09"
+    H="20/04"
 
-    [[ $(date +"%d/%m" 2> /dev/null) == $D ]] && I=""
+    [[ $(date +"%d/%m" 2> /dev/null) == $B ]] && I=""
+    [[ $(date +"%d/%m" 2> /dev/null) == $T ]] && I=""
+    [[ $(date +"%d/%m" 2> /dev/null) == $H ]] && I="󰴺"
 }
 
 echo " $I $D "
