17b079c8c86b1c361e64c7458f5560207d4c9540
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-12 17:51:48 +0200

    Update

--- .config/dmenu/config.h
+++ .config/dmenu/config.h
@@ -6,15 +6,15 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static const char *fonts[] = {
 	"Cascadia Mono NF:size=14"
 };
-static const char *prompt      = "Run:";      /* -p  option; prompt to the left of input field */
+static const char *prompt = "Run:";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
+	/*                  fg         bg     */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeSel]  = { "#eeeeee", "#005577" },
+	[SchemeOut]  = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines = 0;
 
 /*
  * Characters not considered part of a word while deleting words
