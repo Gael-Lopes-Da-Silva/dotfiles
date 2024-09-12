02ca9d4116c97eadf54407f4e87251ba4d3ddaae
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-12 19:34:59 +0200

    Update

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -64,7 +64,7 @@ static const Layout layouts[] = {
 #include <X11/XF86keysym.h>
 
 /* commands */
-static char dmenumon[2] = "-1"; /* component of dmenucmd, manipulated in spawn() */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 
 static const char *dmenucmd[]  = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]   = { "kitty", NULL };
