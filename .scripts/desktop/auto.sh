31b89a3773b493a7d7912824d21d6ee4ab6afb2e
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-12 13:17:04 +0200

    Update

--- .scripts/desktop/auto.sh
+++ .scripts/desktop/auto.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 bash pacman.sh
-bash ly.sh
 bash grub.sh
-bash i3.sh
+bash ly.sh
 bash packages.sh
+bash dwm.sh
+bash dmenu.sh
+# bash i3.sh
---
+++ .scripts/desktop/dmenu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ../../.config/dmenu
+make clean install
