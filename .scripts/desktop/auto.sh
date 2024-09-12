f5550a8445deeb007be86b7e23f846a68b14059c
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-12 19:25:02 +0200

    Update

--- .scripts/desktop/auto.sh
+++ .scripts/desktop/auto.sh
@@ -4,4 +4,3 @@ bash pacman.sh
 bash ly.sh
 bash packages.sh
 bash dwm.sh
-# bash i3.sh
--- .scripts/desktop/i3.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-pacman -S --noconfirm xorg xclip i3-wm i3blocks dmenu maim
-pacman -S --noconfirm upower brightnessctl network-manager-applet
