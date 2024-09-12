0ae25ffbb5a267bd7f050ca301db5e9518d540ea
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-12 23:54:12 +0200

    Update

--- .scripts/utils/steam.sh
+++ .scripts/utils/steam.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pacman -S --noconfirm steam xdg-desktop-portal xdg-desktop-portal-gtk ttf-liberation
+pacman -S --noconfirm steam xdg-desktop-portal xdg-desktop-portal-gtk ttf-liberation lib32-nvidia-utils
