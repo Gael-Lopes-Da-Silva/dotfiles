ff3b5e55e6f814f9a12d64f0ce8eed443dc3c334
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-12 17:47:06 +0200

    Update

--- .config/dwmblocks/blocks.h
+++ .config/dwmblocks/blocks.h
@@ -1,13 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/* {"",            "",             	30,         		0}, */
-	{"",   "$HOME/.config/dwmblocks/scripts/rec.sh",   1,  0},
-	{"",   "$HOME/.config/dwmblocks/scripts/volume.sh",   1,  0},
-	{"",   "$HOME/.config/dwmblocks/scripts/brightness.sh",   1,  0},
-	{"",   "$HOME/.config/dwmblocks/scripts/battery.sh",   1,  0},
-	{"",   "$HOME/.config/dwmblocks/scripts/date.sh",   1,  0},
-	{"",   "$HOME/.config/dwmblocks/scripts/time.sh",   1,  0},
+	/*Icon*/  /*Command*/		                                /*Update Interval*/  /*Update Signal*/
+	{"",      "$HOME/.config/dwmblocks/scripts/rec.sh",         1,                   0},
+	{"",      "$HOME/.config/dwmblocks/scripts/volume.sh",      1,                   0},
+	{"",      "$HOME/.config/dwmblocks/scripts/brightness.sh",  1,                   0},
+	{"",      "$HOME/.config/dwmblocks/scripts/battery.sh",     1,                   0},
+	{"",      "$HOME/.config/dwmblocks/scripts/date.sh",        1,                   0},
+	{"",      "$HOME/.config/dwmblocks/scripts/time.sh",        1,                   0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
