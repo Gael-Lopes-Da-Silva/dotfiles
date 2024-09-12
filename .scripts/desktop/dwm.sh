1bc98ecf8f6ece4826f9472532bc484cb288babb
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-12 14:07:51 +0200

    Update

--- .scripts/desktop/dwm.sh
+++ .scripts/desktop/dwm.sh
@@ -6,4 +6,5 @@ pacman -S --noconfirm upower brightnessctl network-manager-applet
 cd /home/gael/.config/dwm
 make clean install
 
-echo -e "xsetroot -solid \"#474747\"; nm-applet; exec dwm" >> $HOME/.xinitrc
+echo -e "xsetroot -solid \"#474747\"; nm-applet; exec dwm" >> /home/gael/.xinitrc
+chmod 755 /home/gael/.xinitrc
