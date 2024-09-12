c53128f8a7e795d0fa8dd49629d4d3863c4438ad
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-12 16:35:45 +0200

    Update

--- .scripts/desktop/dwm.sh
+++ .scripts/desktop/dwm.sh
@@ -9,5 +9,5 @@ make clean install
 cd /home/gael/.config/dwmblocks
 make clean install
 
-echo -e "xsetroot -solid \"#474747\"; dwmblocks; exec dwm" >> /home/gael/.xinitrc
+echo -e "xsetroot -solid \"#474747\" &; dwmblocks &; exec dwm" >> /home/gael/.xinitrc
 chmod 755 /home/gael/.xinitrc
