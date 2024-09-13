9640bde74dfc45a6b74403be1d95dbb3f4ae4afd
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-13 12:45:29 +0200

    Update to add pango notation in status bar

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -10,7 +10,7 @@ static const int systraypinningfailfirst = 1;       /* 1: if pinning fails, disp
 static const int showsystray             = 1;       /* 0 means no systray */
 static const int showbar                 = 1;       /* 0 means no bar */
 static const int topbar                  = 1;       /* 0 means bottom bar */
-static const char *fonts[]               = { "Cascadia Mono NF:size=14" };
+static const char font[]                 = "Cascadia Mono NF 14";
 static const char dmenufont[]            = "Cascadia Mono NF:size=14";
 static const char col_gray1[]            = "#222222";
 static const char col_gray2[]            = "#444444";
