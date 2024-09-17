7a773b79bd478f10e170c0a0eafa8d3f8c1d4d48
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-17 18:20:07 +0200

    Update install.sh script

--- install.sh
+++ install.sh
@@ -3,136 +3,16 @@
 FG_RESET="\033[0m"
 FG_RED="\033[1;31m"
 FG_GREEN="\033[1;32m"
-FG_BLUE="\033[1;34m"
 FG_GREY="\033[1;37m"
+FG_BLACK="\033[1;30m"
 
-echo -e "${FG_RED}1${FG_RESET} - ${FG_GREY}packages${FG_RESET}  ${FG_GREEN}Install my packages and stow config into user home${FG_RESET}"
-echo -e "${FG_RED}2${FG_RESET} - ${FG_GREY}desktop${FG_RESET}   ${FG_GREEN}Install my desktop and greeter${FG_RESET}"
-echo -e "${FG_RED}3${FG_RESET} - ${FG_GREY}server${FG_RESET}    ${FG_GREEN}Install and setup a server with php and mariadb${FG_RESET}"
-echo -e "${FG_RED}4${FG_RESET} - ${FG_GREY}drivers${FG_RESET}   ${FG_GREEN}List drivers to install${FG_RESET}"
-echo -e "${FG_RED}5${FG_RESET} - ${FG_GREY}other${FG_RESET}     ${FG_GREEN}List other install options${FG_RESET}"
-echo "  -"
-echo -e "${FG_RED}8${FG_RESET} - ${FG_GREY}stow${FG_RESET}      ${FG_GREEN}Only stow config into user home${FG_RESET}"
-echo -e "${FG_RED}9${FG_RESET} - ${FG_GREY}auto${FG_RESET}      ${FG_GREEN}Automatic installation and setup${FG_RESET}"
+write_line () {
+    echo -e "${FG_RED}$1${FG_RESET} - ${FG_GREY}$2${FG_RESET} ${FG_BLACK}$3${FG_RESET}"
+}
 
-echo -en "${FG_BLUE}:${FG_RESET} "
-read INPUT
-
-[[ ${INPUT,,} == "quit" ]] || [[ ${INPUT,,} == "q" ]] && exit
-
-if [[ ! $INPUT =~ ^[0-9]+$ ]]; then
-    echo "ERROR: select valid number"
-    exit
-fi
-
-echo ""
-
-case $INPUT in
-    1)
-        install_packages
-        ;;
-
-    2)
-        install_desktop
-        ;;
-
-    3)
-        install_server
-        ;;
-
-    4)
-        echo -e "${FG_RED}1${FG_RESET} - ${FG_GREY}intel${FG_RESET}   ${FG_GREEN}Install intel graphics dsh rivers${FG_RESET}"
-        echo -e "${FG_RED}2${FG_RESET} - ${FG_GREY}nvidia${FG_RESET}  ${FG_GREEN}Install nvidia proprietary graphics drivers${FG_RESET}"
-
-        echo -en "${FG_BLUE}:${FG_RESET} "
-        read INPUT
-
-        [[ ${INPUT,,} == "quit" ]] || [[ ${INPUT,,} == "q" ]] && exit
-
-        if [[ ! $INPUT =~ ^[0-9]+$ ]]; then
-            echo "ERROR: select valid number"
-            exit
-        fi
-
-        echo ""
-
-        case $INPUT in
-            1)
-                pacman -S --noconfirm mesa vulkan-intel
-                modprobe i915 fastboot=1
-                ;;
-
-            2)
-                pacman -S --noconfirm nvidia nvidia-settings lib32-nvidia-utils
-                sed -i "s|MODULES=()|MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm asus_wmi)|" /etc/mkinitcpio.conf
-                mkdir -p /etc/pacman.d/hooks/
-                echo -e "[Trigger]\nOperation=Install\nOperation=Upgrade\nOperation=Remove\nType=Package\nTarget=nvidia\nTarget=linux\n[Action]\nDescription=Updating NVIDIA module in initcpio\nDepends=mkinitcpio\nWhen=PostTransaction\nNeedsTargets\nExec=/bin/sh -c 'while read -r trg; do case \$trg in linux*) exit 0; esac; done; /usr/bin/mkinitcpio -P'\n" >> /etc/pacman.d/hooks/nvidia.hook
-                nvidia-xconfig
-                echo -e "options nvidia_drm modeset=1 fbdev=1" >> /etc/modprobe.d/nvidia.conf
-                mkinitcpio -P
-                ;;
-        esac
-        ;;
-
-    5)
-        echo -e "${FG_RED}1${FG_RESET} - ${FG_GREY}pacman${FG_RESET}          ${FG_GREEN}Setup pacman to be more usable${FG_RESET}"
-        echo -e "${FG_RED}2${FG_RESET} - ${FG_GREY}grub${FG_RESET}            ${FG_GREEN}Setup grub font${FG_RESET}"
-        echo -e "${FG_RED}3${FG_RESET} - ${FG_GREY}steam${FG_RESET}           ${FG_GREEN}Install and setup steam${FG_RESET}"
-        echo -e "${FG_RED}4${FG_RESET} - ${FG_GREY}v4l2loopback${FG_RESET}    ${FG_GREEN}Install and setup v4l2loopback${FG_RESET}"
-        echo -e "${FG_RED}5${FG_RESET} - ${FG_GREY}virtualization${FG_RESET}  ${FG_GREEN}Install and setup virtualbox${FG_RESET}"
-
-        echo -en "${FG_BLUE}:${FG_RESET} "
-        read INPUT
-
-        [[ ${INPUT,,} == "quit" ]] || [[ ${INPUT,,} == "q" ]] && exit
-
-        if [[ ! $INPUT =~ ^[0-9]+$ ]]; then
-            echo "ERROR: select valid number"
-            exit
-        fi
-
-        echo ""
-
-        case $INPUT in
-            1)
-                setup_pacman
-                ;;
-
-            2)
-                setup_grub
-                ;;
-
-            3)
-                pacman -S --noconfirm steam xdg-desktop-portal xdg-desktop-portal-gtk ttf-liberation
-                ;;
-
-            4)
-                pacman -S --noconfirm v4l2loopback-dkms v4l2loopback-utils linux-headers
-                modprobe v4l2loopback
-                ;;
-
-            5)
-                pacman -S --noconfirm virtualbox virtualbox-guest-iso virtualbox-host-modules-arch
-                modprobe virtio
-                modprobe vboxdrv
-                modprobe vboxnetadp
-                modprobe vboxnetflt
-                usermod -aG vboxusers gael
-                ;;
-        esac
-        ;;
-
-    8)
-        stow /home/gael/.dotfiles/home/
-        ;;
-
-    9)
-        setup_pacman
-        install_packages
-        setup_grub
-        install_desktop
-        ;;
-esac
+write_prompt () {
+    echo -en "${FG_GREEN}$1${FG_RESET} "
+}
 
 install_packages () {
     pacman -S --noconfirm noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk ttf-cascadia-code papirus-icon-theme unzip unrar p7zip stow neovim lazygit nushell feh kitty firefox chromium discord
@@ -185,8 +65,128 @@ setup_pacman () {
     sed -i "s|#VerbosePkgLists|VerbosePkgLists|" /etc/pacman.conf
 }
 
-setup_grub () {
-    grub-mkfont -s 24 -o /boot/grub/fonts/cascadia_mono.pf2 /usr/share/fonts/TTF/CascadiaMono.ttf
-    echo -e 'GRUB_FONT="/boot/grub/fonts/cascadia_mono.pf2"' >> /etc/default/grub
-    grub-mkconfig -o /boot/grub/grub.cfg
+main () {
+    write_line 1 "packages" "Install my packages and stow config into user home"
+    write_line 2 "desktop"  "Install my desktop and greeter"
+    write_line 3 "server"   "Install and setup a server with php and mariadb"
+    write_line 4 "drivers"  "List drivers to install"
+    write_line 5 "other"    "List other install options"
+    echo ""
+    write_line 8 "stow"     "Only stow config into user home"
+    write_line 9 "auto"     "Automatic installation and setup"
+    write_prompt ":"
+
+    read INPUT
+
+    [[ ${INPUT,,} == "quit" ]] || [[ ${INPUT,,} == "q" ]] && exit
+
+    if [[ ! $INPUT =~ ^[0-9]+$ ]]; then
+        echo "ERROR: select valid number"
+        exit
+    fi
+
+    echo ""
+
+    case $INPUT in
+        1)
+            install_packages
+            ;;
+
+        2)
+            install_desktop
+            ;;
+
+        3)
+            install_server
+            ;;
+
+        4)
+            write_line 1 "intel" "Install intel graphics dsh rivers"
+            write_line 2 "nvidia" "Install nvidia proprietary graphics drivers"
+            write_prompt ":"
+
+            read INPUT
+
+            [[ ${INPUT,,} == "quit" ]] || [[ ${INPUT,,} == "q" ]] && exit
+
+            if [[ ! $INPUT =~ ^[0-9]+$ ]]; then
+                echo "ERROR: select valid number"
+                exit
+            fi
+
+            echo ""
+
+            case $INPUT in
+                1)
+                    pacman -S --noconfirm mesa vulkan-intel
+                    modprobe i915 fastboot=1
+                    ;;
+
+                2)
+                    pacman -S --noconfirm nvidia nvidia-settings lib32-nvidia-utils
+                    sed -i "s|MODULES=()|MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm asus_wmi)|" /etc/mkinitcpio.conf
+                    mkdir -p /etc/pacman.d/hooks/
+                    echo -e "[Trigger]\nOperation=Install\nOperation=Upgrade\nOperation=Remove\nType=Package\nTarget=nvidia\nTarget=linux\n[Action]\nDescription=Updating NVIDIA module in initcpio\nDepends=mkinitcpio\nWhen=PostTransaction\nNeedsTargets\nExec=/bin/sh -c 'while read -r trg; do case \$trg in linux*) exit 0; esac; done; /usr/bin/mkinitcpio -P'\n" >> /etc/pacman.d/hooks/nvidia.hook
+                    nvidia-xconfig
+                    echo -e "options nvidia_drm modeset=1 fbdev=1" >> /etc/modprobe.d/nvidia.conf
+                    mkinitcpio -P
+                    ;;
+            esac
+            ;;
+
+        5)
+            write_line 1 "pacman" "Setup pacman to be more usable"
+            write_line 2 "steam" "Install and setup steam"
+            write_line 3 "v4l2loopback" "Install and setup v4l2loopback"
+            write_line 4 "virtualization" "Install and setup virtualbox"
+            write_prompt ":"
+
+            read INPUT
+
+            [[ ${INPUT,,} == "quit" ]] || [[ ${INPUT,,} == "q" ]] && exit
+
+            if [[ ! $INPUT =~ ^[0-9]+$ ]]; then
+                echo "ERROR: select valid number"
+                exit
+            fi
+
+            echo ""
+
+            case $INPUT in
+                1)
+                    setup_pacman
+                    ;;
+
+                2)
+                    pacman -S --noconfirm steam xdg-desktop-portal xdg-desktop-portal-gtk ttf-liberation
+                    ;;
+
+                3)
+                    pacman -S --noconfirm v4l2loopback-dkms v4l2loopback-utils linux-headers
+                    modprobe v4l2loopback
+                    ;;
+
+                4)
+                    pacman -S --noconfirm virtualbox virtualbox-guest-iso virtualbox-host-modules-arch
+                    modprobe virtio
+                    modprobe vboxdrv
+                    modprobe vboxnetadp
+                    modprobe vboxnetflt
+                    usermod -aG vboxusers gael
+                    ;;
+            esac
+            ;;
+
+        8)
+            stow /home/gael/.dotfiles/home/
+            ;;
+
+        9)
+            setup_pacman
+            install_packages
+            install_desktop
+            ;;
+    esac
 }
+
+main
