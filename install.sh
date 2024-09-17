9fb69b120be1eb0eb8915fa35f46a5a2e301f4f1
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-17 18:42:40 +0200

    Update

--- install.sh
+++ install.sh
@@ -40,26 +40,26 @@ install_desktop () {
 
 install_server () {
     pacman -S --noconfirm apache mariadb php php-apache
-    sed -i "s|#LoadModule unique_id_module modules/mod_unique_id.so|LoadModule unique_id_module modules/mod_unique_id.so|" /etc/httpd/conf/httpd.conf
-    sed -i "s|#LoadModule rewrite_module modules/mod_rewrite.so|LoadModule rewrite_module modules/mod_rewrite.so|" /etc/httpd/conf/httpd.conf
-    nvim /etc/httpd/conf/httpd.conf
-    systemctl enable httpd.service
-    systemctl start httpd.service
-    mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-    systemctl enable mariadb.service
-    systemctl start mariadb.service
-    mariadb-secure-installation
-    sed -i "s|LoadModule mpm_event_module modules/mod_mpm_event.so|#LoadModule mpm_event_module modules/mod_mpm_event.so|" /etc/httpd/conf/httpd.conf
-    sed -i "s|#LoadModule mpm_prefork_module modules/mod_mpm_prefork.so|LoadModule mpm_prefork_module modules/mod_mpm_prefork.so|" /etc/httpd/conf/httpd.conf
-    echo -e "LoadModule php_module modules/libphp.so\nAddHandler php-script .php\nInclude conf/extra/php_module.conf" >> /etc/httpd/conf/httpd.conf
-    sed -i "s|;extension=mysqli|extension=mysqli|" /etc/php/php.ini
-    sed -i "s|;extension=pdo_mysql|extension=pdo_mysql|" /etc/php/php.ini
-    sed -i "s|display_errors = Off|display_errors = On|" /etc/php/php.ini
-    sed -i "s|display_startup_errors = Off|display_startup_errors = On|" /etc/php/php.ini
-    sed -i "s|;error_prepend_string = \"<span style='color: #ff0000'>\"|error_prepend_string = \"<span style='color: #ff0000'>\"|" /etc/php/php.ini
-    sed -i "s|;error_append_string = \"</span>\"|error_append_string = \"</span>\"|" /etc/php/php.ini
-    systemctl restart httpd.service
-    systemctl restart mysql.service
+    # sed -i "s|#LoadModule unique_id_module modules/mod_unique_id.so|LoadModule unique_id_module modules/mod_unique_id.so|" /etc/httpd/conf/httpd.conf
+    # sed -i "s|#LoadModule rewrite_module modules/mod_rewrite.so|LoadModule rewrite_module modules/mod_rewrite.so|" /etc/httpd/conf/httpd.conf
+    # nvim /etc/httpd/conf/httpd.conf
+    # systemctl enable httpd.service
+    # systemctl start httpd.service
+    # mariadb-install-db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
+    # systemctl enable mariadb.service
+    # systemctl start mariadb.service
+    # mariadb-secure-installation
+    # sed -i "s|LoadModule mpm_event_module modules/mod_mpm_event.so|#LoadModule mpm_event_module modules/mod_mpm_event.so|" /etc/httpd/conf/httpd.conf
+    # sed -i "s|#LoadModule mpm_prefork_module modules/mod_mpm_prefork.so|LoadModule mpm_prefork_module modules/mod_mpm_prefork.so|" /etc/httpd/conf/httpd.conf
+    # echo -e "LoadModule php_module modules/libphp.so\nAddHandler php-script .php\nInclude conf/extra/php_module.conf" >> /etc/httpd/conf/httpd.conf
+    # sed -i "s|;extension=mysqli|extension=mysqli|" /etc/php/php.ini
+    # sed -i "s|;extension=pdo_mysql|extension=pdo_mysql|" /etc/php/php.ini
+    # sed -i "s|display_errors = Off|display_errors = On|" /etc/php/php.ini
+    # sed -i "s|display_startup_errors = Off|display_startup_errors = On|" /etc/php/php.ini
+    # sed -i "s|;error_prepend_string = \"<span style='color: #ff0000'>\"|error_prepend_string = \"<span style='color: #ff0000'>\"|" /etc/php/php.ini
+    # sed -i "s|;error_append_string = \"</span>\"|error_append_string = \"</span>\"|" /etc/php/php.ini
+    # systemctl restart httpd.service
+    # systemctl restart mysql.service
 }
 
 setup_pacman () {
