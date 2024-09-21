fbc442d34e2d46f03523cc6bcd342ccc4a863c75
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2024-09-21 23:32:45 +0200

    Update dwmblocks scripts

--- home/.config/suckless/dwmblocks/scripts/battery.sh
+++ home/.config/suckless/dwmblocks/scripts/battery.sh
@@ -3,25 +3,33 @@
 B=$(upower -i $(upower -e 2> /dev/null | grep /battery) 2> /dev/null | grep percentage | awk '{print $2}' | sed "s|%||g"); [[ $? -ne 0 ]] || [[ $B == "" ]] && exit
 S=$(upower -i $(upower -e 2> /dev/null | grep /battery) 2> /dev/null | grep state | awk '{print $2}'); [[ $? -ne 0 ]] || [[ $S == "" ]] && exit
 I="󰁹"
+
 T=0
+C=""
 
 {
     [[ $S == "charging" ]] && I="󱐋"
 
     if [[ $S == "charging" ]]; then
-        [[ $B -ge 95 ]] && B="^c#00FF00^$B" && T=1
+        [[ $B -ge 95 ]] && C="#00FF00" && T=1
     fi
 
     if [[ $S == "discharging" ]]; then
-        [[ $B -le 20 ]] && B="^c#FF8000^$B" && T=1
-        [[ $B -le 10 ]] && B="^b#FF0000^$B" && T=1
+        [[ $B -le 20 ]] && C="#FF8000" && T=1
+        [[ $B -le 10 ]] && C="#FF0000" && T=2
     fi
 
-    [[ $B -eq 100 ]] && I="" && B="^b#0000FF^$B" && T=1
+    [[ $B -eq 100 ]] && I="" && C="#0000FF" && T=2
 }
 
-[[ $T -eq 1 ]] && B="$B%^d^" || B="$B%"
+B="$B%"
 
-echo " $I $B "
+if [[ $T -eq 0 ]]; then
+    echo " $I $B "
+elif [[ $T -eq 1 ]]; then
+    echo " $I ^c$C^$B^d^ "
+elif [[ $T -eq 2 ]]; then
+    echo "^b$C^ $I $B ^d^"
+fi
 
 exit 0
