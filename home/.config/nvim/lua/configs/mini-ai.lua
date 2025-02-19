4b0c0c4bb795c0f705fd1db88b125a6ee2bbc3c7
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2025-02-19 21:26:16 +0100

    feat: added more textobjets to nvim

--- home/.config/nvim/lua/configs/mini-ai.lua
+++ home/.config/nvim/lua/configs/mini-ai.lua
@@ -1,3 +1,12 @@
 MiniDeps.later(function()
-	require("mini.ai").setup({})
+    local mini_extra = require("mini.extra")
+    require("mini.ai").setup({
+        custom_textobjects = {
+            B = mini_extra.gen_ai_spec.buffer(),
+            D = mini_extra.gen_ai_spec.diagnostic(),
+            I = mini_extra.gen_ai_spec.indent(),
+            L = mini_extra.gen_ai_spec.line(),
+            N = mini_extra.gen_ai_spec.number(),
+        },
+    })
 end)
