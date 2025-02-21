629f6901008846a7fcb98b05e61b4b451d4929b4
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2025-02-22 00:03:04 +0100

    feat: check for existing soundboard before override

--- home/.scripts/soundboard.sh
+++ home/.scripts/soundboard.sh
@@ -4,15 +4,17 @@ SINK_NAME=SoundboardInput
 SOURCE_NAME=SoundboardOutput
 MIC_SOURCE=$(pactl get-default-source)
 
-pactl unload-module module-null-sink 2> /dev/null
+if [[ $MIC_SOURCE != $SOURCE_NAME ]]; then
+    pactl unload-module module-null-sink 2> /dev/null
 
-pactl load-module module-null-sink media.class=Audio/Sink sink_name=$SINK_NAME device.description=$SINK_NAME channel_map=stereo >> /dev/null
-pactl load-module module-null-sink media.class=Audio/Source/Virtual sink_name=$SOURCE_NAME device.description=$SOURCE_NAME channel_map=front-left,front-right >> /dev/null
+    pactl load-module module-null-sink media.class=Audio/Sink sink_name=$SINK_NAME device.description=$SINK_NAME channel_map=stereo >> /dev/null
+    pactl load-module module-null-sink media.class=Audio/Source/Virtual sink_name=$SOURCE_NAME device.description=$SOURCE_NAME channel_map=front-left,front-right >> /dev/null
 
-pw-link $MIC_SOURCE:capture_FL  $SINK_NAME:playback_FL
-pw-link $MIC_SOURCE:capture_FR  $SINK_NAME:playback_FR
+    pw-link $MIC_SOURCE:capture_FL  $SINK_NAME:playback_FL
+    pw-link $MIC_SOURCE:capture_FR  $SINK_NAME:playback_FR
 
-pw-link $SINK_NAME:monitor_FL $SOURCE_NAME:input_FL
-pw-link $SINK_NAME:monitor_FR $SOURCE_NAME:input_FR
+    pw-link $SINK_NAME:monitor_FL $SOURCE_NAME:input_FL
+    pw-link $SINK_NAME:monitor_FR $SOURCE_NAME:input_FR
 
-pactl set-default-source $SOURCE_NAME
+    pactl set-default-source $SOURCE_NAME
+fi
