01fbba265b0b331ece1256103d2c2595a7a57a7d
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2025-02-23 12:11:22 +0100

    fix: issue with <cr> keymap when completion is active

--- home/.config/nvim/lua/configs/mini-completion.lua
+++ home/.config/nvim/lua/configs/mini-completion.lua
@@ -5,16 +5,4 @@ MiniDeps.later(function()
             signature = { border = "single" },
         },
     })
-
-    vim.keymap.set('i', '<CR>', function()
-        local keycode = vim.keycode or function(x)
-            return vim.api.nvim_replace_termcodes(x, true, true, true)
-        end
-
-        if vim.fn.pumvisible() ~= 0 then
-            return vim.fn.complete_info()['selected'] ~= -1 and keycode('<C-y>') or keycode('<C-y><CR>')
-        else
-            return require("mini.pairs").cr()
-        end
-    end, { expr = true })
 end)
--- home/.config/nvim/lua/configs/mini-pairs.lua
+++ home/.config/nvim/lua/configs/mini-pairs.lua
@@ -5,4 +5,16 @@ MiniDeps.later(function()
             terminal = true
         }
     })
+
+    vim.keymap.set('i', '<CR>', function()
+        local keycode = vim.keycode or function(x)
+            return vim.api.nvim_replace_termcodes(x, true, true, true)
+        end
+
+        if vim.fn.pumvisible() ~= 0 then
+            return vim.fn.complete_info()['selected'] ~= -1 and keycode('<C-y>') or keycode('<C-y><CR>')
+        else
+            return require("mini.pairs").cr()
+        end
+    end, { expr = true })
 end)
