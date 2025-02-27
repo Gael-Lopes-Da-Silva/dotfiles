e8d81966cacf15b67b181c263348753b1425575c
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2025-02-27 15:16:44 +0100

    feat: changed statusline config in nvim

--- home/.config/nvim/lua/configs/mini-statusline.lua
+++ home/.config/nvim/lua/configs/mini-statusline.lua
@@ -3,11 +3,11 @@ MiniDeps.now(function()
     mini_statusline.setup({
         content = {
             active = function()
-                local mode, mode_hl = mini_statusline.section_mode({ trunc_width = 120 })
-                local filename = mini_statusline.section_filename({ trunc_width = 140 })
-                local location = mini_statusline.section_location({ trunc_width = 75 })
-                local search = mini_statusline.section_searchcount({ trunc_width = 75 })
+                local mode, mode_hl = mini_statusline.section_mode({ trunc_width = 100 })
+                local filename = mini_statusline.section_filename({ trunc_width = 100 })
                 local fileinfo = mini_statusline.section_fileinfo({ trunc_width = 999 })
+
+                local location = mini_statusline.is_truncated(100) and '%2l│%-2v' or '%2l/%-2L│%2v/%-2{virtcol("$") - 1}'
 
                 return mini_statusline.combine_groups({
                     {
@@ -26,7 +26,7 @@ MiniDeps.now(function()
                     },
                     {
                         hl = mode_hl,
-                        strings = { search, location },
+                        strings = { location },
                     },
                 })
             end,
