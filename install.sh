71f1209d3c1bca7634ccf649d030510624867cf2
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2025-04-08 11:01:25 +0200

    feat: update install script

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ cd ..
 rm -rf ./paru/
 
 # Packages
-sudo paru -S --noconfirm noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk ttf-cascadia-code ttf-liberation papirus-icon-theme bash-completion ouch stow chromium neovim ripgrep udiskie dunst feh kitty
+sudo pacman -S --noconfirm noto-fonts noto-fonts-extra noto-fonts-emoji noto-fonts-cjk ttf-cascadia-code ttf-liberation papirus-icon-theme bash-completion ouch stow chromium neovim ripgrep udiskie dunst feh kitty
 
 # Stow
 cd /home/$USER/.dotfiles/
@@ -31,7 +31,7 @@ stow home --adopt
 git restore .
 
 # Desktop
-sudo paru -S --noconfirm xorg xorg-xinit xclip xdg-desktop-portal xdg-desktop-portal-gtk maim upower brightnessctl network-manager-applet
+sudo pacman -S --noconfirm xorg xorg-xinit xclip xdg-desktop-portal xdg-desktop-portal-gtk maim upower brightnessctl network-manager-applet
 cd /home/$USER/.dotfiles/home/.config/suckless/dwm
 sudo make clean install
 cd /home/$USER/.dotfiles/home/.config/suckless/dwmblocks
@@ -44,29 +44,29 @@ dconf write /org/gnome/desktop/interface/color-scheme \'prefer-dark\'
 
 # Gpu Drivers
 if lspci | grep -i vga | grep -iq nvidia; then
-    sudo paru -S --noconfirm nvidia
+    sudo pacman -S --noconfirm nvidia
     sudo sed -i '/^MODULES=/ s/)/ nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
     sudo mkinitcpio -P
     sudo nvidia-xconfig
     sudo mkdir /etc/pacman.d/hooks
-    echo -e "[Trigger]\nOperation=Install\nOperation=Upgrade\nOperation=Remove\nType=Package\nTarget=nvidia\nTarget=nvidia-open\nTarget=nvidia-lts\nTarget=linux\n\n[Action]\nDescription=Updating NVIDIA module in initcpio\nDepends=mkinitcpio\nWhen=PostTransaction\nNeedsTargets\nExec=/bin/sh -c 'while read -r trg; do case \$trg in linux*) exit 0; esac; done; /usr/bin/mkinitcpio -P'" | sudo tee /etc/pacman.d/hooks/nvidia.hook > /dev/null
+    echo -e "[Trigger]\nOperation=Install\nOperation=Upgrade\nOperation=Remove\nType=Package\nTarget=nvidia\nTarget=nvidia-open\nTarget=nvidia-lts\nTarget=linux\n\n[Action]\nDescription=Updating NVIDIA module in initcpio\nDepends=mkinitcpio\nWhen=PostTransaction\nNeedsTargets\nExec=/bin/sh -c 'while read -r trg; do case \$trg in linux*) exit 0; esac; done; /usr/bin/mkinitcpio -P'" | sudo tee -a /etc/pacman.d/hooks/nvidia.hook
 fi
 
 if lspci | grep -i vga | grep -iq intel; then
-    sudo paru -S --noconfirm mesa vulkan-intel
+    sudo pacman -S --noconfirm mesa vulkan-intel
 fi
 
 # Loopback
-sudo paru -S --noconfirm v4l2loopback-dkms v4l2loopback-utils
+sudo pacman -S --noconfirm linux-headers v4l2loopback-dkms v4l2loopback-utils
 sudo modprobe v4l2loopback
 
 # Docker
-sudo paru -S --noconfirm docker
+sudo pacman -S --noconfirm docker
 sudo usermod -aG docker $USER
 sudo systemctl enable docker.service
 
 # Virtualization
-sudo paru -S --noconfirm libvirt dnsmasq qemu-full virt-manager virt-viewer
+sudo pacman -S --noconfirm libvirt dnsmasq qemu-full virt-manager virt-viewer
 sudo usermod -aG libvirt $USER
 sudo systemctl enable libvirtd.service
 
