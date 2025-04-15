d04ac6b88db3ef13ac5f2fd849e4d8432dc9f8fa
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2025-04-15 10:58:36 +0200

    feat(nvim): update config

--- home/.config/nvim/lua/configs/config-lsp.lua
+++ home/.config/nvim/lua/configs/config-lsp.lua
@@ -123,4 +123,12 @@ vim.lsp.config("jsonls", {
     },
 })
 
-vim.lsp.config('*', { capabilities = require("mini.completion").get_lsp_capabilities() })
+vim.lsp.enable("zls")
+vim.lsp.config("zls", {
+    cmd = { "zls" },
+    root_markers = { 'zls.json' },
+    filetypes = { "zig", "zir" },
+    single_file_support = true,
+})
+
+-- vim.lsp.config('*', { capabilities = require("mini.completion").get_lsp_capabilities() })
