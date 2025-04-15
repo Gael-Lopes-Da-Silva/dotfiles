adefbec25ad782734773b91290db85b63974718d
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2025-04-15 11:34:22 +0200

    feat(nvim): enabled back mini.snippets

--- home/.config/nvim/lua/configs/mini/mini-snippets.lua
+++ home/.config/nvim/lua/configs/mini/mini-snippets.lua
@@ -1,9 +1,9 @@
--- MiniDeps.later(function()
---     local mini_snippets = require('mini.snippets')
---     mini_snippets.setup({
---         snippets = {
---             mini_snippets.gen_loader.from_file('~/.config/nvim/lua/snippets/global.json'),
---             mini_snippets.gen_loader.from_lang()
---         }
---     })
--- end)
+MiniDeps.later(function()
+    local mini_snippets = require('mini.snippets')
+    mini_snippets.setup({
+        snippets = {
+            mini_snippets.gen_loader.from_file('~/.config/nvim/lua/configs/config-snippets.json'),
+            mini_snippets.gen_loader.from_lang()
+        }
+    })
+end)
