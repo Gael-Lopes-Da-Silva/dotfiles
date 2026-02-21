52fad1450af897186bc1cbb2b4babb78cf9061f1
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2026-02-21 01:42:17 +0100

    feat: update install script

--- install.sh
+++ install.sh
@@ -6,143 +6,149 @@ USER_NAME="$(whoami)"
 DOTFILES_DIR="${HOME}/.dotfiles"
 DOTFILES_REPO="https://github.com/Gael-Lopes-Da-Silva/dotfiles.git"
 
-echo "==> Starting installation..."
+print() {
+    printf "\e[34m%s\e[0m\n" "$*"
+}
+
+print "==> Starting installation..."
 
 # -----------------
 # Distro check
 # -----------------
 if [[ ! -f /etc/os-release ]]; then
-  echo "Unable to detect Linux distribution."
-  exit 1
+    print "Unable to detect Linux distribution."
+    exit 1
 fi
 
 . /etc/os-release
 
 if [[ "$ID" != "arch" && "$ID_LIKE" != *"arch"* ]]; then
-  echo "This script requires an Arch-based distribution."
-  echo "Detected distro: $PRETTY_NAME"
-  exit 1
+    print "This script requires an Arch-based distribution."
+    print "Detected distro: $PRETTY_NAME"
+    exit 1
 fi
-
 
 # -----------------
 # System
 # -----------------
-echo "==> Installing system configuration..."
-sudo pacman -S --noconfirm terminus-font
+print "==> Installing system configuration..."
+{
+    sudo pacman -S --noconfirm terminus-font > /dev/null
 
-echo "FONT=ter-132n" | sudo tee -a /etc/vconsole.conf
+    echo "FONT=ter-132n" | sudo tee -a /etc/vconsole.conf
 
-sudo sed -i "s|#Color|Color|" /etc/pacman.conf
-sudo sed -i "s|#ParallelDownloads = 5|ParallelDownloads = 5|" /etc/pacman.conf
-sudo sed -i "s|#VerbosePkgLists|VerbosePkgLists|" /etc/pacman.conf
-sudo sed -i "s|#HookDir|HookDir|" /etc/pacman.conf
-
+    sudo sed -i "s|#Color|Color|" /etc/pacman.conf
+    sudo sed -i "s|#ParallelDownloads = 5|ParallelDownloads = 5|" /etc/pacman.conf
+    sudo sed -i "s|#VerbosePkgLists|VerbosePkgLists|" /etc/pacman.conf
+    sudo sed -i "s|#HookDir|HookDir|" /etc/pacman.conf
+} > /dev/null
 
 # -----------------
 # Packages
 # -----------------
-echo "==> Installing base packages..."
-sudo pacman -S --noconfirm \
-  noto-fonts \
-  noto-fonts-extra \
-  noto-fonts-emoji \
-  noto-fonts-cjk \
-  git \
-  stow \
-  ouch \
-  alacritty \
-  neovim \
-  firefox \
-  zed \
-  7zip
-
-
-# -----------------
-# Dependencies
-# -----------------
-echo "==> Installing dependencies..."
-sudo pacman -S --noconfirm \
-  accountsservice \
-  power-profiles-daemon \
-  cups-pk-helper \
-  fprintd
-
-echo "=> Enabling related services..."
-sudo systemctl enable --now power-profiles-daemon.service
-sudo systemctl enable --now fprintd.service
-
+print "==> Installing base packages..."
+{
+    sudo pacman -S --noconfirm \
+      noto-fonts \
+      noto-fonts-extra \
+      noto-fonts-emoji \
+      noto-fonts-cjk \
+      git \
+      stow \
+      ouch \
+      alacritty \
+      neovim \
+      firefox \
+      zed \
+      7zip
+} > /dev/null
 
 # -----------------
 # Dotfiles
 # -----------------
-echo "==> Setting up dotfiles..."
+print "==> Setting up dotfiles..."
 
 if [ ! -d "$DOTFILES_DIR" ]; then
-  echo "Cloning dotfiles repository..."
-  git clone "$DOTFILES_REPO" "$DOTFILES_DIR"
+  print "Cloning dotfiles repository..."
+  git clone "$DOTFILES_REPO" "$DOTFILES_DIR" > /dev/null
 else
-  echo "Dotfiles already exist, pulling latest changes..."
-  git -C "$DOTFILES_DIR" pull
+  print "Dotfiles already exist, pulling latest changes..."
+  git -C "$DOTFILES_DIR" pull > /dev/null
 fi
 
-cd "$DOTFILES_DIR"
-stow home --adopt
-git restore .
-
+{
+    cd "$DOTFILES_DIR"
+    stow home --adopt
+    git restore .
+} > /dev/null
 
 # -----------------
 # Desktop
 # -----------------
-echo "==> Installing desktop packages..."
-sudo pacman -S --noconfirm \
-  gtk3 \
-  gtk4 \
-  qt5-wayland \
-  qt6-wayland \
-  xdg-desktop-portal \
-  xdg-desktop-portal-gtk \
-  niri
+print "==> Installing desktop packages..."
+{
+    sudo pacman -S --noconfirm \
+      gtk3 \
+      gtk4 \
+      qt5-wayland \
+      qt6-wayland \
+      xdg-desktop-portal \
+      xdg-desktop-portal-gtk \
+      niri
+} > /dev/null
 
+# -----------------
+# Dependencies
+# -----------------
+print "==> Installing dependencies..."
+{
+    sudo pacman -S --noconfirm \
+      accountsservice \
+      power-profiles-daemon \
+      cups-pk-helper \
+      fprintd
+
+    sudo systemctl enable --now power-profiles-daemon.service
+    sudo systemctl enable --now fprintd.service
+} > /dev/null
 
 # -----------------
 # Drivers
 # -----------------
-echo "==> Installing drivers..."
+print "==> Installing drivers..."
 
 if lspci | grep -i vga | grep -iq nvidia; then
-  echo "=> NVIDIA GPU detected"
-  sudo pacman -S --noconfirm nvidia-dkms
+  print "NVIDIA GPU detected"
+  sudo pacman -S --noconfirm nvidia-dkms > /dev/null
 
-  sudo sed -i '/^MODULES=/ s/)/ nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
+  sudo sed -i '/^MODULES=/ s/)/ nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf > /dev/null
 
-  sudo mkinitcpio -P
+  sudo mkinitcpio -P > /dev/null
 elif lspci | grep -i vga | grep -iq intel; then
-  echo "=> Intel GPU detected"
-  sudo pacman -S --noconfirm mesa vulkan-intel
+  print "Intel GPU detected"
+  sudo pacman -S --noconfirm mesa vulkan-intel > /dev/null
 fi
-
 
 # -----------------
 # Programming
 # -----------------
-echo "==> Installing programming tools..."
+print "==> Installing programming tools..."
+{
+    sudo pacman -S --noconfirm \
+      linux-headers \
+      v4l2loopback-dkms \
+      v4l2loopback-utils
 
-sudo pacman -S --noconfirm \
-  linux-headers \
-  v4l2loopback-dkms \
-  v4l2loopback-utils
+    sudo modprobe v4l2loopback
 
-sudo modprobe v4l2loopback
+    sudo pacman -S --noconfirm nodejs npm
+    sudo npm install -g bun
 
-sudo pacman -S --noconfirm nodejs npm
-sudo npm install -g bun
+    sudo pacman -S --noconfirm php composer
 
-sudo pacman -S --noconfirm php composer
+    sudo pacman -S --noconfirm docker docker-compose
+    sudo usermod -aG docker "${USER_NAME}"
+    sudo systemctl enable --now docker.service
+} > /dev/null
 
-sudo pacman -S --noconfirm docker docker-compose
-sudo usermod -aG docker "${USER_NAME}"
-sudo systemctl enable --now docker.service
-
-
-echo "==> Installation complete!"
+print "==> Installation complete!"
