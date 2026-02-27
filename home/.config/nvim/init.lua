d06a6332e2c81c06715ef63db964e3df7344a1c4
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2026-02-27 10:42:00 +0100

    feat(nvim): removed treesitter and added mini.cmdline

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,11 +1,11 @@
-local package_path = vim.fn.stdpath("data") .. "/site/"
-local snapshot_path = vim.fn.stdpath("config") .. "/snapshot/"
+local package_path = vim.fn.stdpath("data") .. "/site"
+local snapshot_path = vim.fn.stdpath("config") .. "/snapshot"
 local log_path = vim.fn.stdpath("data") .. "/logs/mini-deps.log"
 local mini_path = package_path .. "pack/deps/start/mini.nvim"
 
 if vim.fn.isdirectory(mini_path) == 0 then
     vim.cmd('echo "Installing `mini.nvim`" | redraw')
-    vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/echasnovski/mini.nvim", mini_path })
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/nvim-mini/mini.nvim", mini_path })
     vim.cmd("packadd mini.nvim | helptags ALL")
     vim.cmd('echo "Installed `mini.nvim`" | redraw')
 end
--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -1,11 +1,6 @@
-MiniDeps.later(function() -- treesitter
-    MiniDeps.add({
-        source = "nvim-treesitter/nvim-treesitter",
-        hooks = {
-            post_checkout = function()
-                vim.cmd("TSUpdate")
-            end,
-        },
-    })
-    require("plugins.plugin-treesitter")
-end)
+-- MiniDeps.later(function()
+--     MiniDeps.add({
+--         source = "",
+--     })
+--     require("plugins.plugin-")
+-- end)
