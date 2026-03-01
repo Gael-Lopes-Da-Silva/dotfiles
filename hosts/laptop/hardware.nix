43366688fd10bed99290d314bc64ba95150f0afe
Gaël <gael.lopes-da-silva@outlook.fr>
2026-03-01 02:28:31 +0100

    Update hardware configuration for boot and file systems

--- hosts/laptop/hardware.nix
+++ hosts/laptop/hardware.nix
@@ -1,32 +1,32 @@
 { config, lib, pkgs, modulsPath, ... }:
 
 {
-  # boot = {
-  #   initrd = {
-  #     availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
-  #     kernelModules = [ ];
-  #   };
+  boot = {
+    initrd = {
+      availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
+      kernelModules = [ ];
+    };
 
-  #   kernelModules = [ "kvm-intel" ];
-  #   extraModulePackages = [ ];
-  # };
+    kernelModules = [ "kvm-intel" ];
+    extraModulePackages = [ ];
+  };
 
-  # fileSystems = {
-  #   "/" = {
-  #     device = "/dev/disk/by-uuid/a0287aac-a5ac-4693-8a64-35bd4e21651a";
-  #     fsType = "ext4";
-  #   };
-  #   "/boot" = {
-  #     device = "/dev/disk/by-uuid/977B-ECBD";
-  #     fsType = "vfat";
-  #     options = [ "fmask=0077" "dmask=0077" ];
-  #   };
-  # };
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/40585011-2613-4085-b537-390ee471d7ed";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-uuid/4291-A4B0";
+      fsType = "vfat";
+      options = [ "fmask=0077" "dmask=0077" ];
+    };
+  };
 
-  # swapDevices = [
-  #   { device = "/dev/disk/by-uuid/da8ea830-72db-4bf1-b694-756fd69b6d99"; }
-  # ];
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/6be0214d-2a5e-4938-92f2-a5b85760d677"; }
+  ];
 
-  # nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  # hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableReditributableFirmware;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableReditributableFirmware;
 }
