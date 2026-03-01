c6a017646a0ad795dd0b0dd324002397e2406da6
Gaël <gael.lopes-da-silva@outlook.fr>
2026-03-01 02:38:26 +0100

    Fix typo in updateMicrocode configuration

--- hosts/laptop/hardware.nix
+++ hosts/laptop/hardware.nix
@@ -28,5 +28,5 @@
   ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableReditributableFirmware;
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
