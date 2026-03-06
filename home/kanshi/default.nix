c096b847e04506b29c5a9126a2f19ba585f039e7
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2026-03-06 13:16:17 +0100

    feat: changed from niri output settings to kanshi

---
+++ home/kanshi/default.nix
@@ -0,0 +1,63 @@
+{ config, pkgs, ... }:
+
+{
+  services.kanshi = {
+    enable = true;
+    systemdTarget = "graphical-session.target";
+
+    settings = [
+      {
+        profile.name = "laptop_undocked";
+
+        profile.outputs = [
+          {
+            criteria = "eDP-1";
+            mode = "1920x1080@60Hz";
+            scale = 1.0;
+            status = "enable";
+          }
+        ];
+      }
+      {
+        profile.name = "laptop_docked";
+
+        profile.outputs = [
+          {
+            criteria = "eDP-1";
+            mode = "1920x1080@60Hz";
+            position = "0,1080";
+            scale = 1.0;
+            status = "enable";
+          }
+          {
+            criteria = "HDMI-A-1";
+            mode = "1920x1080@60Hz";
+            position = "0,0";
+            scale = 1.0;
+            status = "enable";
+          }
+        ];
+      }
+      {
+        profile.name = "desktop";
+
+        profile.outputs = [
+          {
+            criteria = "HDMI-A-1";
+            mode = "1920x1080@100Hz";
+            position = "0,1080";
+            scale = 1.0;
+            status = "enable";
+          }
+          {
+            criteria = "HDMI-A-2";
+            mode = "1920x1080@60Hz";
+            position = "0,0";
+            scale = 1.0;
+            status = "enable";
+          }
+        ];
+      }
+    ];
+  };
+}
