da3d81deea132e0141f32368141aaf13cced6e64
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2026-03-06 20:17:34 +0100

    fix: audio crackling

--- modules/audio.nix
+++ modules/audio.nix
@@ -11,29 +11,38 @@
     wireplumber.enable = true;
 
     extraConfig = {
-      pipewire."91-null-sinks" = {
-        "context.objects" = [
-          {
-            factory = "adapter";
-            args = {
-              "factory.name"     = "support.null-audio-sink";
-              "node.name"        = "SoundboardSink";
-              "node.description" = "Soundboard Speaker";
-              "media.class"      = "Audio/Sink";
-              "audio.position"   = "FL,FR";
-            };
-          }
-          {
-            factory = "adapter";
-            args = {
-              "factory.name"     = "support.null-audio-sink";
-              "node.name"        = "SoundboardSource";
-              "node.description" = "Soundboard Mic";
-              "media.class"      = "Audio/Source/Virtual";
-              "audio.position"   = "FL,FR";
-            };
-          }
-        ];
+      pipewire = {
+        "context.properties" = {
+          "default.clock.rate" = 48000;
+          "default.clock.quantum" = 2048;
+          "default.clock.min-quantum" = 2048;
+          "default.clock.max-quantum" = 8192;
+        };
+
+        "91-null-sinks" = {
+          "context.objects" = [
+            {
+              factory = "adapter";
+              args = {
+                "factory.name"     = "support.null-audio-sink";
+                "node.name"        = "SoundboardSink";
+                "node.description" = "Soundboard Speaker";
+                "media.class"      = "Audio/Sink";
+                "audio.position"   = "FL,FR";
+              };
+            }
+            {
+              factory = "adapter";
+              args = {
+                "factory.name"     = "support.null-audio-sink";
+                "node.name"        = "SoundboardSource";
+                "node.description" = "Soundboard Mic";
+                "media.class"      = "Audio/Source/Virtual";
+                "audio.position"   = "FL,FR";
+              };
+            }
+          ];
+        };
       };
     };
   };
