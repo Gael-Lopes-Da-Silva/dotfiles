446902b93c559963680626c093230a220be38e1f
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2026-03-20 16:32:20 +0100

    feat: save

--- home/scripts/menu/cliphist_menu.sh
+++ home/scripts/menu/cliphist_menu.sh
@@ -4,21 +4,27 @@ if pgrep -f "$TERMINAL.*--class custom:cliphist" >/dev/null; then
     exit 0
 fi
 
+tmpfile=$(mktemp)
+
 $TERMINAL --class custom:cliphist -e bash -c '
     selection=$(
         {
-            echo "__clear__:🧹 Clear"
-            cliphist list | sed "s/^/__item__:/"
+            printf "__clear__\t🧹 Clear\n"
+            cliphist list \
+            | sort -t $'\''\t'\'' -k1,1nr \
+            | while IFS=$'\''\t'\'' read -r id text; do
+                printf "__item__\t%s\t%s\n" "$text" "$id"
+            done
         } | fzf \
             --no-sort \
             --prompt="Copy: " \
-            --delimiter=":" \
+            --delimiter=$'\''\t'\'' \
             --with-nth=2 \
             --layout=reverse
     )
 
-    key=$(printf "%s" "$selection" | cut -d":" -f1)
-    value=$(printf "%s" "$selection" | cut -d":" -f2-)
+    key=$(printf "%s" "$selection" | cut -d$'\''\t'\'' -f1)
+    value=$(printf "%s" "$selection" | cut -d$'\''\t'\'' -f3)
 
     case "$key" in
         __clear__)
@@ -30,9 +36,17 @@ $TERMINAL --class custom:cliphist -e bash -c '
                 "Clipboard history" "The clipboard history was successfully cleared."
             ;;
         __item__)
-            [ -n "$value" ] && printf "%s" "$value" | cliphist decode | wl-copy
+            [ -n "$value" ] && printf "%s" "$value" > "'$tmpfile'"
             ;;
     esac
 '
+
+fzf_output=$(cat "$tmpfile")
+rm "$tmpfile"
+
+item_to_copy=$fzf_output
+if [ -n "$item_to_copy" ]; then
+    printf "%s" $item_to_copy | cliphist decode | wl-copy
+fi
 
 exit 0
