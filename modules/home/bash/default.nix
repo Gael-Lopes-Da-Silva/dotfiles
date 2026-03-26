1f78eb8a9c70caa59fb8f1439e11628ce3a36694
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2026-03-26 23:48:12 +0100

    feat: save

--- modules/home/bash/default.nix
+++ modules/home/bash/default.nix
@@ -53,7 +53,7 @@
       "exit"
     ];
 
-    bashrcExtra = ''
+    initExtra = ''
       bind "set completion-ignore-case on"
       bind "set show-all-if-ambiguous on"
       bind "TAB:menu-complete"
