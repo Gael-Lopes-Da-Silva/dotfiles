8a7eeca57321c2b6ddd8b6476cd31cb3eebdf686
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2026-03-28 21:37:14 +0100

    Added environment variables to Ollama config

--- home/ollama/default.nix
+++ home/ollama/default.nix
@@ -5,5 +5,10 @@
     enable = true;
     port = 11434;
     host = "127.0.0.1";
+    environmentVariables = {
+      OLLAMA_NUM_PARALLEL = 4;
+      OLLAMA_MAX_LOADED_MODELS = 1;
+      OLLAMA_KEEP_ALIVE = "10m";
+    };
   };
 }
