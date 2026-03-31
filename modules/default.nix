c5d87194e504123ded139435d5cb5124544f6881
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2026-03-31 12:36:06 +0200

    Silly

--- modules/default.nix
+++ modules/default.nix
@@ -148,11 +148,12 @@
 
     udev = {
       packages = with pkgs; [
-        qmk
-        qmk-udev-rules
-        qmk_hid
         via
         vial
+
+        qmk
+        qmk_hid
+        qmk-udev-rules
       ];
     };
 
