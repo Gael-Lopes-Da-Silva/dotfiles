8a121f46a9cfcd3dd2a1f24b6c9addc70473da49
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2026-03-31 12:09:43 +0200

    Added an utility script to freeze the process of the current window

--- home/scripts/default.nix
+++ home/scripts/default.nix
@@ -20,5 +20,7 @@
   home.file.".local/bin/brightness_monitor.sh".source = ./monitor/brightness_monitor.sh;
   home.file.".local/bin/output_monitor.sh".source = ./monitor/output_monitor.sh;
 
+  home.file.".local/bin/freeze_process.sh".source = ./utility/freeze_process.sh;
+
   home.file.".local/bin/soundboard_setup.sh".source = ./setup/soundboard_setup.sh;
 }
