ed6bc3d100f5d84051370a891a92daae2378bc4d
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2026-04-09 12:02:08 +0200

    Added sounds assets in .local

--- home/default.nix
+++ home/default.nix
@@ -25,6 +25,7 @@
       ./ollama
       ./qt
       ./scripts
+      ./sounds
       ./udiskie
       ./zed
 
---
+++ home/sounds/default.nix
@@ -0,0 +1,8 @@
+{ ... }:
+
+{
+  home.file.".local/sounds/windows-11-startup.mp3".source = ./assets/windows-11-startup.mp3;
+  home.file.".local/sounds/windows-11-usb-disconnect.mp3".source =
+    ./assets/windows-11-usb-disconnect.mp3;
+  home.file.".local/sounds/windows-11-usb-insert.mp3".source = ./assets/windows-11-usb-insert.mp3;
+}
