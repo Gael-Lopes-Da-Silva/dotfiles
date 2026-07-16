ec5538f79f2cf37fddcae6dbc89c808097b45706
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2026-07-16 15:08:28 +0200

    Minor refactor

--- home/sounds/default.nix
+++ home/sounds/default.nix
@@ -1,8 +1,18 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  home.file.".local/sounds/windows-11-notify.mp3".source = ./windows-11-notify.mp3;
-  home.file.".local/sounds/windows-11-startup.mp3".source = ./windows-11-startup.mp3;
-  home.file.".local/sounds/windows-11-usb-disconnect.mp3".source = ./windows-11-usb-disconnect.mp3;
-  home.file.".local/sounds/windows-11-usb-insert.mp3".source = ./windows-11-usb-insert.mp3;
+  home.file =
+    pkgs.lib.mapAttrs'
+      (filename: srcPath: {
+        name = ".local/sounds/${filename}";
+        value = {
+          source = srcPath;
+        };
+      })
+      {
+        "windows-11-notify.mp3" = ./windows-11-notify.;
+        "windows-11-startup.mp3" = ./windows-11-startup.mp3.;
+        "windows-11-usb-disconnect.mp3" = ./windows-11-usb-disconnect.mp3.;
+        "windows-11-usb-insert.mp3" = ./windows-11-usb-insert.mp3.;
+      };
 }
