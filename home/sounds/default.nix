a03d4b4294336008cd20ad433e991ac10dd4513c
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2026-07-16 15:10:04 +0200

    Typo

--- home/sounds/default.nix
+++ home/sounds/default.nix
@@ -10,7 +10,7 @@
         };
       })
       {
-        "windows-11-notify.mp3" = ./windows-11-notify.;
+        "windows-11-notify.mp3" = ./windows-11-notify.mp3;
         "windows-11-startup.mp3" = ./windows-11-startup.mp3.;
         "windows-11-usb-disconnect.mp3" = ./windows-11-usb-disconnect.mp3.;
         "windows-11-usb-insert.mp3" = ./windows-11-usb-insert.mp3.;
