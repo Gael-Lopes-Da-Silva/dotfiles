c820c283d6e42447d1dfe5bed9a97182a7bbbadf
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2026-07-20 09:17:57 +0200

    Setup wireguard on the work station

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -1,11 +1,37 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
     ./hardware.nix
   ];
 
-  networking.hostName = "windows11";
+  environment.systemPackages = with pkgs; [
+    wireguard-tools
+  ];
+
+  networking = {
+    hostName = "windows11";
+
+    wg-quick.interfaces.wg0 = {
+      address = [ "10.8.0.3/32" ];
+      dns = [ "10.0.0.1" ];
+      mtu = 1420;
+
+      privateKeyFile = "/home/gael/.config/wireguard/gael.key";
+
+      peers = [
+        {
+          publicKey = "Bqt6sSxpSYf7lpon+ufSsgxxeVRykffmgnbrSBOnzT8=";
+          endpoint = "46.105.79.118:51820";
+          allowedIPs = [
+            "10.0.0.0/16"
+            "10.8.0.0/24"
+          ];
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
 
   services = {
     tlp.enable = true;
