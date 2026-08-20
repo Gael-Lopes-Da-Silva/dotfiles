e4f76f4b252eea53362b6ee208782ddadad04e79
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2026-08-20 09:18:17 +0200

    Update mako

--- home/configs/mako.nix
+++ home/configs/mako.nix
@@ -9,13 +9,13 @@
       default-timeout = 5000;
       text-color = "#ffffffff";
       background-color = "#0D0D0Dff";
-      border-size = 6;
+      border-size = 0;
       border-color = "#3B3B3Bff";
       border-radius = 10;
       progress-color = "#242424ff";
       margin = 2;
-      padding = 8;
-      outer-margin = 20;
+      padding = 10;
+      outer-margin = 14;
 
       "app-name=osd".anchor = "bottom-center";
       "app-name=notification".anchor = "top-right";
