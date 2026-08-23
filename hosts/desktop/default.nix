f32dea5036527d6deea59a289da53dae3c8230e5
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2026-08-23 22:57:49 +0200

    Udev rule to remove ps4 controller audio

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -25,5 +25,11 @@
     ACTION=="add|change", KERNEL=="event[0-9]*", \
       ATTRS{name}=="Wireless Controller Touchpad", \
       ENV{LIBINPUT_IGNORE_DEVICE}="1"
+
+    ACTION=="add", SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_interface", \
+      ATTRS{idVendor}=="054c", \
+      ATTRS{idProduct}=="05c4|09cc", \
+      DRIVER=="snd-usb-audio", \
+      RUN+="/bin/sh -c 'echo %k > /sys/bus/usb/drivers/snd-usb-audio/unbind'"
   '';
 }
