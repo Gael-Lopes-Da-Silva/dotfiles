037ead640a79998a9988fdae623502dd7a811c46
gael-lopes-da-silva <gael.lopes-da-silva@outlook.fr>
2026-08-31 11:28:48 +0200

    Remove llama-cpp

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   imports = [
@@ -6,10 +6,6 @@
   ];
 
   networking.hostName = "windows11";
-
-  environment.systemPackages = with pkgs; [
-    llama-cpp-vulkan
-  ];
 
   programs.steam = {
     enable = true;
--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -7,7 +7,6 @@
   ];
 
   environment.systemPackages = with pkgs; [
-    llama-cpp
     cursor-cli
   ];
 
